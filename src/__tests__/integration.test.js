e3e75f0ebee552a9e7929c20ab79a513b5cc5b38
Joeri Malmberg <joeri@mycujoo.tv>
2019-02-19 09:46:03 +0100

    Added integration test, added error catching for the kafka consumer and logging of that error.

---
+++ src/__tests__/integration.test.js
@@ -0,0 +1,89 @@
+'use strict'
+
+const _ = require('lodash')
+const config = require('config')
+const got = require('got')
+const mongodbUri = require('mongodb-uri')
+const { MongoClient } = require('mongodb')
+const logger = require('@mycujoo/logger')
+const { Producer } = require('@mycujoo/kafka-clients')
+
+const { applyMagic } = require('../')
+const uploadSchema = require('../uploadSchema')
+
+const topic = `avro_mongodb_test-${Date.now()}_v2`
+
+const schemaSet = {
+  uniqueProps: ['id'],
+  modelName: 'Followers',
+  topic,
+}
+const schemaSets = [schemaSet]
+
+const gotInstance = got.extend({
+  baseUrl: `http://localhost:${config.server.port}/api/v1`,
+  json: true,
+})
+
+async function waitABit(abit) {
+  await new Promise(resolve => {
+    setTimeout(async () => {
+      resolve()
+    }, abit)
+  })
+}
+
+beforeAll(async () => {
+  await uploadSchema(topic, require('./schema1.json'))
+  const uri = mongodbUri.format(config.mongodb)
+  const connection = await MongoClient.connect(uri)
+  const db = await connection.db(config.mongodb.database)
+  await db.dropDatabase()
+  await connection.close()
+  const { start, stop } = await applyMagic(logger, schemaSets, config)
+  await start()
+
+  afterAll(async () => {
+    await stop()
+  })
+})
+
+describe('The avro mongodb api tests', () => {
+  it('should create a message on kafka and retrieve it via the api', async () => {
+    jest.setTimeout(15000)
+    const producer = new Producer(_.extend({ topic }, config.kafka))
+
+    await new Promise(resolve => {
+      producer.once('ready', resolve)
+    })
+
+    await producer.produce({
+      id: 'test1',
+      previousId: null,
+      follows: [{ id: 'follow1', type: 'team' }],
+    })
+
+    await waitABit(3000)
+
+    const query = JSON.stringify({
+      id: 'test1',
+    })
+
+    const res = await gotInstance.get(`Followers?query=${query}`)
+
+    let follow = res.body[0]
+
+    // Omit random generated _id props
+    follow = _.omit(follow, '_id')
+    follow.follows = _.map(follow.follows, follow => {
+      return _.omit(follow, '_id')
+    })
+
+    expect(follow).toEqual({
+      id: 'test1',
+      __v: 0,
+      follows: [{ id: 'follow1', type: 'team' }],
+      previousId: null,
+    })
+  })
+})
